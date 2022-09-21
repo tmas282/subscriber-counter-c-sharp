3dbf6c43b9235fab0f502f30606f37160f9a7de7
tmas282 <tomaspintomoreira28@gmail.com>
2022-09-21 23:04:12 +0100

    primeiro commit v1.0

---
+++ SubscriberCounterCsharp/SubscriberCounter.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SubscriberCounterCsharp
+{
+    partial class SubscriberCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SubscriberCounter));
+            this.lblSubscriberCounter = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSubscriberCounter
+            // 
+            this.lblSubscriberCounter.AutoSize = true;
+            this.lblSubscriberCounter.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubscriberCounter.ForeColor = System.Drawing.Color.White;
+            this.lblSubscriberCounter.Location = new System.Drawing.Point(127, 9);
+            this.lblSubscriberCounter.Name = "lblSubscriberCounter";
+            this.lblSubscriberCounter.Size = new System.Drawing.Size(540, 60);
+            this.lblSubscriberCounter.TabIndex = 0;
+            this.lblSubscriberCounter.Text = "Subscriber Counter";
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(181, 160);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(426, 39);
+            this.txtId.TabIndex = 1;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Font = new System.Drawing.Font("Showcard Gothic", 28.2F);
+            this.btnConfirmar.Location = new System.Drawing.Point(222, 205);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(328, 102);
+            this.btnConfirmar.TabIndex = 2;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.BtnConfirmar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Showcard Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(315, 362);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(149, 30);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // SubscriberCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblSubscriberCounter);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SubscriberCounter";
+            this.Text = "Subscriber Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSubscriberCounter;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ SubscriberCounterCsharp/SubscriberCounter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SubscriberCounterCsharp
+{
+    public partial class SubscriberCounter : Form
+    {
+        public SubscriberCounter()
+        {
+            InitializeComponent();
+        }
+        private void BtnConfirmar_Click(object sender, EventArgs e)
+        {
+            //UCUfP26mozkiTLZj5eBpFPeQ
+            string api_sub = String.Format("https://www.googleapis.com/youtube/v3/channels?part=statistics&id={0}&key=AIzaSyAIGiSI7O4YubN7RO9DwiqWQdu4l-OGD4Q", txtId.Text);
+            HttpWebRequest getInfo = (HttpWebRequest)WebRequest.Create(api_sub);
+            HttpWebResponse resposta = (HttpWebResponse)getInfo.GetResponse();
+            Stream stream = resposta.GetResponseStream();
+            var jsonEncodificado = new StreamReader(stream);
+            JObject json = JObject.Parse(jsonEncodificado.ReadToEnd());
+            var subs = json["items"][0]["statistics"]["subscriberCount"];
+            lblResultado.Text = String.Format("{0}", subs);
+        }
+    }
+}
